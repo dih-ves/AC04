8ce7d29ce341fa198c2bbc26bf7f0f60e7befd29
Diego Galves <53906295+dih-ves@users.noreply.github.com>
2019-09-13 21:44:59 -0300

    Update test_operacoes.py

--- Farina/test/test_operacoes.py
+++ Farina/test/test_operacoes.py
@@ -5,4 +5,4 @@ class TestOperacoes(TestCase):
 	def setUp(self):
 		self.operacoes = Operacoes()
 	def test_some(self):
-		self.assertEqual(self.operacoes.soma({1,5]),6, "Should be 6")
+		self.assertEqual(self.operacoes.soma([1,5]),6, "Should be 6")
