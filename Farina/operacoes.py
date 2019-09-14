f627f7133e07dc89acb4ccc3bb5164e4c191652e
Diego Galves <53906295+dih-ves@users.noreply.github.com>
2019-09-13 22:04:22 -0300

    ajustando operações

--- Farina/operacoes.py
+++ Farina/operacoes.py
@@ -2,11 +2,11 @@ class Operacoes():
 	def soma(self, valores):
 		val = 0
 		for v in valores:
-			val = val + val
+			val = val + v
 		return val
 	
 	def subtracao(self, valores):
 		val = 0 
 		for v in valores:
-			val = val - val
+			val = val - v
 		return val
